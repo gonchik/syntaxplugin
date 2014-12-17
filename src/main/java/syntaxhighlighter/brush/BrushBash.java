0c86fa93922764be4892ab0307b48f9032667836
hski <holger@schimanski-web.de>
2014-12-17 20:19:40 +0100

    Bugfix wget in small caps

--- src/main/java/syntaxhighlighter/brush/BrushBash.java
+++ src/main/java/syntaxhighlighter/brush/BrushBash.java
@@ -48,7 +48,7 @@ public class BrushBash extends Brush {
             + "sleep sort source split ssh strace su sudo sum symlink sync tail tar tee test time "
             + "times touch top traceroute trap tr true tsort tty type ulimit umask umount unalias "
             + "uname unexpand uniq units unset unshar useradd usermod users uuencode uudecode v vdir "
-            + "vi watch wc whereis which who whoami Wget xargs yes chsh";
+            + "vi watch wc whereis which who whoami wget xargs yes chsh";
 
     List<RegExpRule> _regExpRuleList = new ArrayList<RegExpRule>();
     _regExpRuleList.add(new RegExpRule("^#!.*$", Pattern.MULTILINE, "preprocessor"));
